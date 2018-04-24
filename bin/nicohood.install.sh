6389cce7e6372fbb703d9246ee2dabdee8ece496
NicoHood <git@nicohood.de>
2018-04-24 20:12:14 +0200

    Generate mirrorlist in install script

--- bin/nicohood.install.sh
+++ bin/nicohood.install.sh
@@ -83,8 +83,9 @@ msg "2 Installation"
 
 # Mirror selection
 msg2 "2.1 Select the mirrors"
-warning "System mirrorlist will be used for new installation."
-plain "Please visit https://www.archlinux.org/mirrorlist/ and only use https + ipv4&6 only mirrors."
+cp "${MOUNT}/etc/pacman.d/mirrorlist" "${MOUNT}/etc/pacman.d/mirrorlist.bak"
+curl -s "https://www.archlinux.org/mirrorlist/?country=DE&country=GB&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on" \
+    | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors - > "${MOUNT}/etc/pacman.d/mirrorlist"
 
 # Install basic system and chroot
 msg2 "2.2 Install the base packages"
